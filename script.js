5e310b9b2c53a556132c5f9e98f4e75f38ce5b52
Soyeb Suvo <105710935+soyebsuvo@users.noreply.github.com>
2022-11-04 03:32:45 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var login = document.querySelector("#login-btn");
+var contact = document.querySelector("#contact-btn");
+var btnPara = document.querySelector("#btnpara");
+
+function login1(){
+    btnPara.innerHTML = "opps sorry! Not avaiable now";
+}
+
+function contact1(){
+    btnPara.innerHTML = "Email: soyebbeen@gmail.com";
+}
